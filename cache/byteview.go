13978e7d9fe25cdaa0b2d37960c67a5b1298ecfa
limerence-yu <2629370046@qq.com>
2025-01-04 16:44:30 +0800

    抽象缓存值

---
+++ cache/byteview.go
@@ -0,0 +1,17 @@
+package cache
+
+type ByteView struct {
+	b []byte
+}
+
+func (v ByteView) Len() int {
+	return len(v.b)
+}
+
+func (v ByteView) String() string {
+	return string(v.b)
+}
+
+func (v ByteView) ByteSlice() []byte {
+	return append([]byte{}, v.b...)
+}
